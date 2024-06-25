8075942d58ab4d71a6c537cd79f197dd50ffe3de
Renan Couto <renanmv10@hotmail.com>
2024-06-25 20:44:30 -0300

    Reusing instantiation

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -10,7 +10,10 @@ public class Progam {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 
